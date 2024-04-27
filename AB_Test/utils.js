af0c5c92b637dfc75ded59b9dffcc6ff3a21747d
rebelwcause <76130565+rebelwcause@users.noreply.github.com>
2024-04-27 16:55:24 -0700

    Update utils.js

--- AB_Test/utils.js
+++ AB_Test/utils.js
@@ -4,46 +4,72 @@
 
 const data = {
 	"Ads": { //Ads
-		"Google Ads": [
-			"https://pagead2.googlesyndication.com", "https://adservice.google.com",
-			"https://pagead2.googleadservices.com"
-		],
-		"Media.net": ["https://static.media.net", "https://media.net", "https://adservetx.media.net"],
-		"Doubleclick.net": ["https://doubleclick.net/", "https://ad.doubleclick.net/", "https://static.doubleclick.net", "https://m.doubleclick.net",
+		"Google Ads": ["https://pagead2.googlesyndication.com",
+			"https://adservice.google.com",
+			"https://pagead2.googleadservices.com"],
+		"Media.net": ["https://static.media.net",
+			"https://media.net",
+			"https://adservetx.media.net"],
+		"Doubleclick.net": ["https://doubleclick.net",
+			"https://ad.doubleclick.net",
+			"https://static.doubleclick.net",
+			"https://m.doubleclick.net",
 			"https://mediavisor.doubleclick.net"
 		],
-		"Amazon": ["https://adtago.s3.amazonaws.com", "https://analyticsengine.s3.amazonaws.com",
-			"https://advertising-api-eu.amazon.com", "https://amazonclix.com"
-		],
-		"Microsoft": ["https://acdn.adnxs.com", "https://acdn.adnxs-simple.com", "https://bat.bing.com"]
+		"Amazon": ["https://adtago.s3.amazonaws.com",
+			"https://analyticsengine.s3.amazonaws.com",
+			"https://advertising-api-eu.amazon.com",
+			"https://amazonclix.com"],
+		"Microsoft": ["https://acdn.adnxs.com",
+			"https://acdn.adnxs-simple.com",
+			"https://bat.bing.com"]
 	},
-	"Analytics": { //Analytics
-		"Google Analytics": ["https://google-analytics.com", "https://ssl.google-analytics.com"],
-		"Hotjar": ["https://hotjar.com", "https://static.hotjar.com"],
-		"MouseFlow": ["https://mouseflow.com/"],
-		"FreshMarketer": "https://freshmarketer.com/",
-		"Luckyorange": ["https://luckyorange.com", "https://cdn.luckyorange.com", "https://w1.luckyorange.com",
-			"https://upload.luckyorange.net", "https://cs.luckyorange.net",
-			"https://settings.luckyorange.net"
-		],
-		"Stats WP Plugin": "https://stats.wp.com"
+	"Analytics": {
+		"Google Analytics": ["https://google-analytics.com",
+			"https://ssl.google-analytics.com"],
+		"Hotjar": ["https://hotjar.com",
+			"https://static.hotjar.com"],
+		"MouseFlow": ["https://mouseflow.com"],
+		"FreshMarketer": "https://freshmarketer.com",
+		"Luckyorange": ["https://luckyorange.com",
+			"https://cdn.luckyorange.com",
+			"https://w1.luckyorange.com",
+			"https://upload.luckyorange.net",
+			"https://cs.luckyorange.net",
+			"https://settings.luckyorange.net"],
+		"Stats WP Plugin": ["https://stats.wp.com"]
 	},
-	"Error Trackers": { //Error tracker
-		"Bugsnag": ["https://notify.bugsnag.com", "https://sessions.bugsnag.com", "https://api.bugsnag.com", "https://app.bugsnag.com"],
-		"Sentry": ["https://browser.sentry-cdn.com", "https://app.getsentry.com"]
+	"Error Trackers": {
+		"Bugsnag": ["https://notify.bugsnag.com",
+			"https://sessions.bugsnag.com",
+			"https://api.bugsnag.com",
+			"https://app.bugsnag.com"],
+		"Sentry": ["https://browser.sentry-cdn.com",
+			"https://app.getsentry.com"]
 	},
 	"Social Trackers": {
-		"Facebook": ["https://pixel.facebook.com", "https://ads.facebook.com", "https://an.facebook.com"],
-		"Twitter": ["https://ads-api.twitter.com", "https://advertising.twitter.com"],
-		"LinkedIn": ["https://ads.linkedin.com", "https://analytics.pointdrive.linkedin.com"],
-		"Pinterest": ["https://ads.pinterest.com", "https://log.pinterest.com", "https://ads-dev.pinterest.com",
-			"https://analytics.pinterest.com", "https://trk.pinterest.com", "https://widgets.pinterest.com"
-		],
-		"Reddit": ["https://ads.reddit.com", "https://d.reddit.com", "https://rereddit.com",
-			"https://events.redditmedia.com"
-		],
+		"Facebook": ["https://pixel.facebook.com",
+			"https://ads.facebook.com",
+			"https://an.facebook.com"],
+		"Twitter": ["https://ads-api.twitter.com",
+			"https://advertising.twitter.com"],
+		"LinkedIn": ["https://ads.linkedin.com",
+			"https://analytics.pointdrive.linkedin.com"],
+		"Pinterest": ["https://ads.pinterest.com",
+			"https://log.pinterest.com",
+			"https://ads-dev.pinterest.com",
+			"https://analytics.pinterest.com",
+			"https://trk.pinterest.com",
+			"https://widgets.pinterest.com"],
+		"Reddit": ["https://ads.reddit.com",
+			"https://d.reddit.com",
+			"https://rereddit.com",
+			"https://events.redditmedia.com"],
 		"YouTube": ["https://ads.youtube.com"],
-		"TikTok": ["https://analytics.tiktok.com", "https://ads.tiktok.com", "https://analytics-sg.tiktok.com", "https://ads-sg.tiktok.com"]
+		"TikTok": ["https://analytics.tiktok.com",
+			"https://ads.tiktok.com",
+			"https://analytics-sg.tiktok.com",
+			"https://ads-sg.tiktok.com"]
 	}
 };
 
@@ -52,11 +78,17 @@ async function check_url(url, div, parent, np) {
 	// Lets set up our `AbortController`, and create a request options object
 	// that includes the controller's `signal` to pass to `fetch`.
 	const controller = new AbortController();
+	//const config = {
+	//	...{
+	//		method: 'HEAD',
+	//		mode: 'no-cors'
+	//	},
+	//	signal: controller.signal
+	//};
+
 	const config = {
-		...{
-			method: 'HEAD',
-			mode: 'no-cors'
-		},
+		method: 'HEAD',
+		mode: 'no-cors',
 		signal: controller.signal
 	};
 	// Set a timeout limit for the request using `setTimeout`. If the body
@@ -69,7 +101,7 @@ async function check_url(url, div, parent, np) {
 	var hostDiv = document.createElement("div");
 	hostDiv.innerHTML = url;
 	div.appendChild(hostDiv);
-	await fetch(url, config, timeout, parent, div).then(response => {
+	await fetch(url + "/fakepage.html", config, timeout, parent, div).then(response => {
 			console.log(response);
 			hostDiv.style.cssText = "background-color: #C70D2C; color: #FFF;";
 		}) //Response was received --> ads are NOT blocked
@@ -92,7 +124,6 @@ async function fetchTests()
 		catEl.innerHTML = "<h3>" + "&nbsp;&nbsp;" + element + "</h3>";
 		testingInfoLoading.appendChild(catEl);
 		var category = data[element];
-		var testInfo = document.createElement("div");
 		for (let key in category)
 		{
 			//Set log test
