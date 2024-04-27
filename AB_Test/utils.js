4aa60b7c2fd89f19dc8cd8915d8c772a989be212
rebelwcause <76130565+rebelwcause@users.noreply.github.com>
2024-04-26 20:54:05 -0700

    Update utils.js

--- AB_Test/utils.js
+++ AB_Test/utils.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 function removeWaitingContent() {
   let className = "testcase-waiting-content";
   let elements = document.getElementsByClassName(className);
@@ -9,28 +9,36 @@ function removeWaitingContent() {
   }
 }
 
-function expectedParameter() {
-  let url = new URL(window.location.href);
-  return url.searchParams.get("expected") == "1";
+
+function expectedParameter()
+{
+	let url = new URL(window.location.href);
+	return url.searchParams.get("expected") == "1";
 }
 
-function expectedPageView() {
-  let documents = [document];
-  while (documents.length > 0) {
-    let doc = documents.shift();
-    doc.body.classList.add("expected-view");
-    let {frames} = doc.defaultView;
-    for (let i = 0; i < frames.length; i++) {
-      try {
-        documents.push(frames[i].document);
-        frames[i].document.defaultView.addEventListener("DOMContentLoaded", expectedPageView);
-      }
-      catch (e) {}
-    }
-  }
+function expectedPageView()
+{
+	let documents = [document];
+	while (documents.length > 0)
+	{
+		let doc = documents.shift();
+		doc.body.classList.add("expected-view");
+		let {frames} = doc.defaultView;
+		for (let i = 0; i < frames.length; i++)
+		{
+			try
+			{
+				documents.push(frames[i].document);
+				frames[i].document.defaultView.addEventListener("DOMContentLoaded", expectedPageView);
+			}
+			catch (e) {}
+		}
+	}
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  if (expectedParameter())
-    expectedPageView();
+	if (expectedParameter())
+		expectedPageView();
 });
+
+*/
