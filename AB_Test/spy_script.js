f0c69ad45db893b55ac7a6441ed7cb8c287b6857
rebelwcause <76130565+rebelwcause@users.noreply.github.com>
2024-04-28 09:35:18 -0700

    Add files via upload

---
+++ AB_Test/spy_script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+/* jshint esversion: 9 */
+
+window.onload = function() {
+	let ab_spy_test_element = document.getElementById("spy-script-div");
+	ab_spy_test_element.setAttribute("data-expectedresult", "fail");
+};
