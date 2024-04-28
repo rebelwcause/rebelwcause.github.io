7be5cabc6c304893a888b15f8849849174dbbd84
rebelwcause <76130565+rebelwcause@users.noreply.github.com>
2024-04-28 09:39:53 -0700

    Update spy_script.js

--- AB_Test/spy_script.js
+++ AB_Test/spy_script.js
@@ -5,4 +5,5 @@
 window.onload = function() {
 	let ab_spy_test_element = document.getElementById("spy-script-div");
 	ab_spy_test_element.setAttribute("data-expectedresult", "fail");
+	ab_spy_test_element.textContent = "Target";
 };
